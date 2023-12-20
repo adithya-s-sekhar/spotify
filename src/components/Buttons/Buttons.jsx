7f379d5ba07146638052e59c5a912e90118c5ed3
Adithya S Sekhar <adithya0332@gmail.com>
2023-12-20 17:31:31 +0530

    YourLibrary: header: add icons

--- src/components/Buttons/Buttons.jsx
+++ src/components/Buttons/Buttons.jsx
@@ -6,12 +6,26 @@ export const IconWithText = (props) => {
 
     return ( 
         <div className='icon-with-text'>
-            <div className='icon-svg'>
+            <div className='icon-with-text-svg'>
                 {icon_svg}
             </div>
-            <div className='icon-text'>
+            <div className='icon-with-text-text'>
                 {icon_text}
             </div>
         </div>
      );
 }
+
+export const Icon = (props) => {
+const icon_svg = props.icon;
+
+    return ( 
+        <div className='icon'>
+            <div className='icon-svg'>
+                {icon_svg}
+            </div>
+        </div>
+     );
+}
+ 
+export default Icon;
--- src/components/YourLibrary/YourLibrary.jsx
+++ src/components/YourLibrary/YourLibrary.jsx
@@ -1,8 +1,12 @@
-import { IconWithText } from '../Buttons/Buttons';
+import Icon, { IconWithText } from '../Buttons/Buttons';
 import './YourLibrary.css';
 
 const YourLibrary = () => {
     const library_svg = <svg data-encore-id="icon" role="img" aria-hidden="true" viewBox="0 0 24 24" class="Svg-sc-ytk21e-0 iYxpxA"><path d="M3 22a1 1 0 0 1-1-1V3a1 1 0 0 1 2 0v18a1 1 0 0 1-1 1zM15.5 2.134A1 1 0 0 0 14 3v18a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V6.464a1 1 0 0 0-.5-.866l-6-3.464zM9 2a1 1 0 0 0-1 1v18a1 1 0 1 0 2 0V3a1 1 0 0 0-1-1z"></path></svg>
+
+    const plus_svg = <svg data-encore-id="icon" role="img" aria-hidden="true" viewBox="0 0 16 16" class="Svg-sc-ytk21e-0 kPpCsU"><path d="M15.25 8a.75.75 0 0 1-.75.75H8.75v5.75a.75.75 0 0 1-1.5 0V8.75H1.5a.75.75 0 0 1 0-1.5h5.75V1.5a.75.75 0 0 1 1.5 0v5.75h5.75a.75.75 0 0 1 .75.75z"></path></svg>
+
+    const arrow_svg = <svg data-encore-id="icon" role="img" aria-hidden="true" viewBox="0 0 16 16" class="Svg-sc-ytk21e-0 kgVuXA"><path d="M7.19 1A.749.749 0 0 1 8.47.47L16 7.99l-7.53 7.521a.75.75 0 0 1-1.234-.815.75.75 0 0 1 .174-.243l5.72-5.714H.75a.75.75 0 1 1 0-1.498h12.38L7.41 1.529a.749.749 0 0 1-.22-.53z"></path></svg>
 
     return ( 
         <div className='your-library'>
@@ -14,7 +18,12 @@ const YourLibrary = () => {
                     />
                 </div>
                 <div className='your-library-header-icons'>
-                    
+                    <Icon
+                        icon = {plus_svg}
+                    />
+                    <Icon
+                        icon = {arrow_svg}
+                    />
                 </div>
             </div>
         </div>
