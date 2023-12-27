9803dec5896d6a8e9dfc4644ec0dc6b70309c062
Adithya S Sekhar <adithya0332@gmail.com>
2023-12-27 08:54:31 +0530

    AlbumCard: text styles and play button

--- src/components/AlbumCard/AlbumCard.jsx
+++ src/components/AlbumCard/AlbumCard.jsx
@@ -7,6 +7,7 @@ const AlbumCard = (props) => {
         <div className="album-card">
             <div className="album-card-art">
                 <img src={album.albumart} />
+                <svg viewBox="0 0 24 24"><path d="m7.05 3.606 13.49 7.788a.7.7 0 0 1 0 1.212L7.05 20.394A.7.7 0 0 1 6 19.788V4.212a.7.7 0 0 1 1.05-.606z"></path></svg>
             </div>
             <div className='album-card-details'>
                 <div className="album-card-title">
