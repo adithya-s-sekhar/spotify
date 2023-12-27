371ed320a3b2e9eab1cd328f255b134323d717ea
Adithya S Sekhar <adithya0332@gmail.com>
2023-12-27 11:44:30 +0530

    Main: Add more rows

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,22 +3,82 @@ import AlbumRow from '../AlbumRow/AlbumRow';
 import './Main.css';
 
 const Main = () => {
-    const items = [{
-        name: 'Revengers (Original Motion Picture Soundtrack)',
-        albumart: 'albumart.jpg',
-        artists: 'Hans Dimmer'
-    },{
-        name: 'SuperheroMovie 2: Revenge of Someone (Original Motion Picture Soundtrack)',
-        albumart: 'albumart.jpg',
-        artists: 'Various Artists'
-    }]
+    const recentlyPlayed = [
+        {
+            name: 'Revengers (Original Motion Picture Soundtrack)',
+            albumart: 'albumart.jpg',
+            artists: 'Hans Dimmer'
+        },
+        {
+            name: 'SuperheroMovie 2: Revenge of Someone (Original Motion Picture Soundtrack)',
+            albumart: 'albumart.jpg',
+            artists: 'Various Artists'
+        }
+    ]
+
+    const madeForYou = [
+        {
+            name: 'Revengers (Original Motion Picture Soundtrack)',
+            albumart: 'albumart.jpg',
+            artists: 'Hans Dimmer'
+        },
+        {
+            name: 'SuperheroMovie 2: Revenge of Someone (Original Motion Picture Soundtrack)',
+            albumart: 'albumart.jpg',
+            artists: 'Various Artists'
+        },
+        {
+            name: 'Galactic Odyssey',
+            albumart: 'albumart.jpg',
+            artists: 'Stellar Ensemble'
+        },
+        {
+            name: 'Lost in Time',
+            albumart: 'albumart.jpg',
+            artists: 'Chrono Wave'
+        },
+        {
+            name: 'Epic Journeys',
+            albumart: 'albumart.jpg',
+            artists: 'Quest Soundtrack'
+        },
+        {
+            name: 'City Lights',
+            albumart: 'albumart.jpg',
+            artists: 'Urban Grooves Collective'
+        },
+        {
+            name: 'Voyage to the Unknown',
+            albumart: 'albumart.jpg',
+            artists: 'Mystery Explorers'
+        },
+        {
+            name: 'Serenade of the Stars',
+            albumart: 'albumart.jpg',
+            artists: 'Celestial Harmony Orchestra'
+        }
+    ]
+
+
 
     return ( 
         <div className='main'>
             <Navbar />
             <AlbumRow 
                 header = "Recently Played"
-                items = {items}
+                items = {recentlyPlayed}
+            />
+            <AlbumRow 
+                header = "Made For You"
+                items = {madeForYou}
+            />
+            <AlbumRow 
+                header = "Popular albums"
+                items = {madeForYou}
+            />
+            <AlbumRow 
+                header = "Discover picks for you"
+                items = {madeForYou}
             />
         </div>
      );
