75a7c50a55bf7249ce5e482957034fc346dfd5ed
Adithya S Sekhar <adithya0332@gmail.com>
2023-12-29 19:13:00 +0530

    Navbox: Add link to Home

--- src/components/NavBox/NavBox.jsx
+++ src/components/NavBox/NavBox.jsx
@@ -1,5 +1,6 @@
 import './NavBox.css';
 import { IconWithText } from '../Buttons/Buttons';
+import { Link } from 'react-router-dom';
 
 const NavBox = () => {
     const home_svg = <svg viewBox="0 0 24 24"><path d="M12.5 3.247a1 1 0 0 0-1 0L4 7.577V20h4.5v-6a1 1 0 0 1 1-1h5a1 1 0 0 1 1 1v6H20V7.577l-7.5-4.33zm-2-1.732a3 3 0 0 1 3 0l7.5 4.33a2 2 0 0 1 1 1.732V21a1 1 0 0 1-1 1h-6.5a1 1 0 0 1-1-1v-6h-3v6a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V7.577a2 2 0 0 1 1-1.732l7.5-4.33z"></path></svg>
@@ -9,16 +10,17 @@ const NavBox = () => {
     return ( 
         <div className='nav-box'>
             <div className='nav-box-content'>
-            <IconWithText
-                icon = {home_svg}
-                text = 'Home'
-            />
-            <IconWithText
-                icon = {search_svg}
-                text = 'Search'
-            />
+                <Link to='/'>
+                    <IconWithText
+                        icon = {home_svg}
+                        text = 'Home'
+                    />
+                </Link>
+                <IconWithText
+                    icon = {search_svg}
+                    text = 'Search'
+                />
             </div>
-            
         </div>
      );
 }
