c76fd213792c4e7f7e45c4fb79f33609638f9b65
Adithya S Sekhar <adithya0332@gmail.com>
2023-12-29 14:16:05 +0530

    Refactor: AlbumDetails -> AlbumSong

--- src/components/AlbumDetails/AlbumDetails.jsx
+++ src/components/AlbumDetails/AlbumDetails.jsx
@@ -1,26 +1,5 @@
 import './AlbumDetails.css';
-
-const AlbumSong = ({index, songTitle, songArtist, songDur}) => {
-
-    return (
-        <div className="album-body-song">
-            <div className="album-body-song-index">
-                {index}
-            </div>
-            <div className="album-body-song-details">
-                <div className="album-body-song-title">
-                    {songTitle}
-                </div>
-                <div className="album-body-song-artist">
-                    {songArtist}
-                </div>
-            </div>
-            <div className="album-body-song-dur">
-                {songDur}
-            </div>
-        </div>
-    );
-}
+import AlbumSong from '../AlbumSong/AlbumSong';
 
 const AlbumDetails = (props) => {
     const album = props.album;
---
+++ src/components/AlbumSong/AlbumSong.jsx
@@ -0,0 +1,24 @@
+import './AlbumSong.css';
+
+const AlbumSong = ({index, songTitle, songArtist, songDur}) => {
+    return (
+        <div className="album-body-song">
+            <div className="album-body-song-index">
+                {index}
+            </div>
+            <div className="album-body-song-details">
+                <div className="album-body-song-title">
+                    {songTitle}
+                </div>
+                <div className="album-body-song-artist">
+                    {songArtist}
+                </div>
+            </div>
+            <div className="album-body-song-dur">
+                {songDur}
+            </div>
+        </div>
+    );
+}
+
+export default AlbumSong;
