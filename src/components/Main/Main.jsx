5dde7f215d73b9ca7d3fa8cdef98297c8c951774
Adithya S Sekhar <adithya0332@gmail.com>
2023-12-29 11:05:41 +0530

    Main: Show AlbumDetails temporarily

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,12 +2,17 @@ import Navbar from '../Navbar/Navbar';
 import './Main.css';
 import Footer from '../Footer/Footer';
 import Home from '../Home/Home';
+import AlbumDetails from '../AlbumDetails/AlbumDetails';
+import { recentlyPlayed } from '../../albums';
 
 const Main = () => {
     return ( 
         <div className='main'>
             <Navbar />
-            <Home />
+            {/* <Home /> */}
+            <AlbumDetails
+                album = {recentlyPlayed[0]}
+            />
             <Footer />
         </div>
      );
