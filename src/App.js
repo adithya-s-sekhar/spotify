3ea9cd72b710bab8f69a0a8fb18f81bdb6ede501
Adithya S Sekhar <adithya0332@gmail.com>
2023-12-29 19:11:44 +0530

    Deconstruct Main into App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import './App.css';
-import Main from './components/Main/Main';
 import NavBox from './components/NavBox/NavBox';
 import NowPlaying from './components/NowPlaying/NowPlaying';
 import YourLibrary from './components/YourLibrary/YourLibrary';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
+import AlbumDetails from './components/AlbumDetails/AlbumDetails';
+import { albums } from './albums';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
     <div className="app">
       <div className='app-wrapper'>
         <NavBox />
         <YourLibrary />
-        <Main />
+        <div className='main'>
+          <Navbar />
+            <Routes>
+                <Route exact path = "/" element = { <Home albums = {albums} /> }/>
+                <Route exact path = "/album/:id" element = { <AlbumDetails albums = {albums} /> }/>
+            </Routes>
+          <Footer />
+        </div>
         <NowPlaying />
       </div>
     </div>
+    </Router>
   );
 }
 
