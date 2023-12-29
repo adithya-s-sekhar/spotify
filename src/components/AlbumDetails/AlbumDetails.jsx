ee9835938c6bcfd1bc52d3b6dfea576de83cb249
Adithya S Sekhar <adithya0332@gmail.com>
2023-12-29 11:06:18 +0530

    AlbumDetails: Add songs

--- src/components/AlbumDetails/AlbumDetails.jsx
+++ src/components/AlbumDetails/AlbumDetails.jsx
@@ -1,5 +1,27 @@
 import './AlbumDetails.css';
 
+const AlbumSong = ({index, songTitle, songArtist, songDur}) => {
+
+    return (
+        <div className="album-body-song">
+            <div className="album-body-song-index">
+                {index}
+            </div>
+            <div className="album-body-song-details">
+                <div className="album-body-song-title">
+                    {songTitle}
+                </div>
+                <div className="album-body-song-artist">
+                    {songArtist}
+                </div>
+            </div>
+            <div className="album-body-song-dur">
+                {songDur}
+            </div>
+        </div>
+    );
+}
+
 const AlbumDetails = (props) => {
     const album = props.album;
 
@@ -29,7 +51,48 @@ const AlbumDetails = (props) => {
                 </div>
             </div>
             <div className="album-body">
-
+                <div className="album-body-controls">
+                    <div className="album-body-controls-left">
+                        <div className="album-body-controls-play">
+                            <svg viewBox="0 0 24 24"><path d="m7.05 3.606 13.49 7.788a.7.7 0 0 1 0 1.212L7.05 20.394A.7.7 0 0 1 6 19.788V4.212a.7.7 0 0 1 1.05-.606z"></path></svg>
+                        </div>
+                        <div className="album-body-controls-heart">
+                            <svg viewBox="0 0 24 24"><path d="M5.21 1.57a6.757 6.757 0 0 1 6.708 1.545.124.124 0 0 0 .165 0 6.741 6.741 0 0 1 5.715-1.78l.004.001a6.802 6.802 0 0 1 5.571 5.376v.003a6.689 6.689 0 0 1-1.49 5.655l-7.954 9.48a2.518 2.518 0 0 1-3.857 0L2.12 12.37A6.683 6.683 0 0 1 .627 6.714 6.757 6.757 0 0 1 5.21 1.57zm3.12 1.803a4.757 4.757 0 0 0-5.74 3.725l-.001.002a4.684 4.684 0 0 0 1.049 3.969l.009.01 7.958 9.485a.518.518 0 0 0 .79 0l7.968-9.495a4.688 4.688 0 0 0 1.049-3.965 4.803 4.803 0 0 0-3.931-3.794 4.74 4.74 0 0 0-4.023 1.256l-.008.008a2.123 2.123 0 0 1-2.9 0l-.007-.007a4.757 4.757 0 0 0-2.214-1.194z"></path></svg>
+                        </div>
+                        <div className="album-body-controls-overflow">
+                            <svg viewBox="0 0 24 24"><path d="M5.21 1.57a6.757 6.757 0 0 1 6.708 1.545.124.124 0 0 0 .165 0 6.741 6.741 0 0 1 5.715-1.78l.004.001a6.802 6.802 0 0 1 5.571 5.376v.003a6.689 6.689 0 0 1-1.49 5.655l-7.954 9.48a2.518 2.518 0 0 1-3.857 0L2.12 12.37A6.683 6.683 0 0 1 .627 6.714 6.757 6.757 0 0 1 5.21 1.57zm3.12 1.803a4.757 4.757 0 0 0-5.74 3.725l-.001.002a4.684 4.684 0 0 0 1.049 3.969l.009.01 7.958 9.485a.518.518 0 0 0 .79 0l7.968-9.495a4.688 4.688 0 0 0 1.049-3.965 4.803 4.803 0 0 0-3.931-3.794 4.74 4.74 0 0 0-4.023 1.256l-.008.008a2.123 2.123 0 0 1-2.9 0l-.007-.007a4.757 4.757 0 0 0-2.214-1.194z"></path></svg>
+                        </div>
+                    </div>
+                    <div className="album-body-controls-right">
+                        <div className="album-body-controls-view">
+                            <svg viewBox="0 0 16 16"><path d="M15 14.5H5V13h10v1.5zm0-5.75H5v-1.5h10v1.5zM15 3H5V1.5h10V3zM3 3H1V1.5h2V3zm0 11.5H1V13h2v1.5zm0-5.75H1v-1.5h2v1.5z"></path></svg>
+                        </div>
+                    </div>
+                </div>
+                <div className="album-body-songs">
+                    <div className="album-body-songs-header">
+                        <div className="album-body-songs-index-header">
+                            #
+                        </div>
+                        <div className="album-body-songs-title-header">
+                            Title
+                        </div>
+                        <div className="album-body-songs-dur-header">
+                            <svg viewBox="0 0 16 16"><path d="M8 1.5a6.5 6.5 0 1 0 0 13 6.5 6.5 0 0 0 0-13zM0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8z"></path><path d="M8 3.25a.75.75 0 0 1 .75.75v3.25H11a.75.75 0 0 1 0 1.5H7.25V4A.75.75 0 0 1 8 3.25z"></path></svg>
+                        </div>
+                    </div>
+                    <div className="album-body-songs-list">
+                        {album.songs.map((song, index) => (
+                            <AlbumSong
+                                key={index}
+                                index={index + 1}
+                                songTitle={song.songTitle}
+                                songArtist={song.songArtist}
+                                songDur={song.songDur}
+                            />
+                        ))}
+                    </div>
+                </div>
             </div>
             <div className="album-footer">
                 {album.longDate}
