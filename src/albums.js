e80bc07cded0d7a25fecb17f17f9df8974e72a5a
Adithya S Sekhar <adithya0332@gmail.com>
2023-12-29 09:14:32 +0530

    export albums

---
+++ src/albums.js
@@ -0,0 +1,55 @@
+export const recentlyPlayed = [
+    {
+        name: 'Revengers (Original Motion Picture Soundtrack)',
+        albumart: 'albumart.jpg',
+        artists: 'Hans Dimmer'
+    },
+    {
+        name: 'SuperheroMovie 2: Revenge of Someone (Original Motion Picture Soundtrack)',
+        albumart: 'albumart.jpg',
+        artists: 'Various Artists'
+    }
+]
+
+export const madeForYou = [
+    {
+        name: 'Galactic Odyssey',
+        albumart: 'albumart.jpg',
+        artists: 'Stellar Ensemble'
+    },
+    {
+        name: 'Lost in Time',
+        albumart: 'albumart.jpg',
+        artists: 'Chrono Wave'
+    },
+    {
+        name: 'Epic Journeys',
+        albumart: 'albumart.jpg',
+        artists: 'Quest Soundtrack'
+    },
+    {
+        name: 'City Lights',
+        albumart: 'albumart.jpg',
+        artists: 'Urban Grooves Collective'
+    },
+    {
+        name: 'Voyage to the Unknown',
+        albumart: 'albumart.jpg',
+        artists: 'Mystery Explorers'
+    },
+    {
+        name: 'Serenade of the Stars',
+        albumart: 'albumart.jpg',
+        artists: 'Celestial Harmony Orchestra'
+    },
+    {
+        name: 'Dreamscapes',
+        albumart: 'albumart.jpg',
+        artists: 'Somnolent Symphony'
+    },
+    {
+        name: 'Rhythms of the Cosmos',
+        albumart: 'albumart.jpg',
+        artists: 'Cosmic Percussionists'
+    }
+]
