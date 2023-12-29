a0e4e77b917d45577ed4680e079600c93bb85d5b
Adithya S Sekhar <adithya0332@gmail.com>
2023-12-29 12:18:59 +0530

    AlbumDetails: styles 2/x: Header and controls

--- src/components/AlbumDetails/AlbumDetails.jsx
+++ src/components/AlbumDetails/AlbumDetails.jsx
@@ -40,12 +40,10 @@ const AlbumDetails = (props) => {
                     </div>
                     <div className="album-header-details">
                         <div className="album-header-artist-image">
-                            <div className="artist-image-mask">
-                                <img src={album.artistart} />
-                            </div>
+                                <img src={album.artistArt} />
                         </div>
                         <div className="album-header-summary">
-                            {album.artist} • {album.year} • {(album.songs).length}
+                            {album.artist}&nbsp;&nbsp;•&nbsp;&nbsp;{album.year}&nbsp;&nbsp;•&nbsp;&nbsp;{(album.songs).length} songs
                         </div>
                     </div>
                 </div>
@@ -60,7 +58,7 @@ const AlbumDetails = (props) => {
                             <svg viewBox="0 0 24 24"><path d="M5.21 1.57a6.757 6.757 0 0 1 6.708 1.545.124.124 0 0 0 .165 0 6.741 6.741 0 0 1 5.715-1.78l.004.001a6.802 6.802 0 0 1 5.571 5.376v.003a6.689 6.689 0 0 1-1.49 5.655l-7.954 9.48a2.518 2.518 0 0 1-3.857 0L2.12 12.37A6.683 6.683 0 0 1 .627 6.714 6.757 6.757 0 0 1 5.21 1.57zm3.12 1.803a4.757 4.757 0 0 0-5.74 3.725l-.001.002a4.684 4.684 0 0 0 1.049 3.969l.009.01 7.958 9.485a.518.518 0 0 0 .79 0l7.968-9.495a4.688 4.688 0 0 0 1.049-3.965 4.803 4.803 0 0 0-3.931-3.794 4.74 4.74 0 0 0-4.023 1.256l-.008.008a2.123 2.123 0 0 1-2.9 0l-.007-.007a4.757 4.757 0 0 0-2.214-1.194z"></path></svg>
                         </div>
                         <div className="album-body-controls-overflow">
-                            <svg viewBox="0 0 24 24"><path d="M5.21 1.57a6.757 6.757 0 0 1 6.708 1.545.124.124 0 0 0 .165 0 6.741 6.741 0 0 1 5.715-1.78l.004.001a6.802 6.802 0 0 1 5.571 5.376v.003a6.689 6.689 0 0 1-1.49 5.655l-7.954 9.48a2.518 2.518 0 0 1-3.857 0L2.12 12.37A6.683 6.683 0 0 1 .627 6.714 6.757 6.757 0 0 1 5.21 1.57zm3.12 1.803a4.757 4.757 0 0 0-5.74 3.725l-.001.002a4.684 4.684 0 0 0 1.049 3.969l.009.01 7.958 9.485a.518.518 0 0 0 .79 0l7.968-9.495a4.688 4.688 0 0 0 1.049-3.965 4.803 4.803 0 0 0-3.931-3.794 4.74 4.74 0 0 0-4.023 1.256l-.008.008a2.123 2.123 0 0 1-2.9 0l-.007-.007a4.757 4.757 0 0 0-2.214-1.194z"></path></svg>
+                        <svg viewBox="0 0 24 24"><path d="M4.5 13.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zm15 0a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zm-7.5 0a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"></path></svg>
                         </div>
                     </div>
                     <div className="album-body-controls-right">
