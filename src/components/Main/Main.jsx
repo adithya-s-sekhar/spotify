85d560b6d622c1788f635d4df6a6517457772279
Adithya S Sekhar <adithya0332@gmail.com>
2023-12-29 09:07:53 +0530

    Refactor: Main -> Home

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,86 +1,13 @@
 import Navbar from '../Navbar/Navbar';
-import AlbumRow from '../AlbumRow/AlbumRow';
 import './Main.css';
 import Footer from '../Footer/Footer';
+import Home from '../Home/Home';
 
 const Main = () => {
-    const recentlyPlayed = [
-        {
-            name: 'Revengers (Original Motion Picture Soundtrack)',
-            albumart: 'albumart.jpg',
-            artists: 'Hans Dimmer'
-        },
-        {
-            name: 'SuperheroMovie 2: Revenge of Someone (Original Motion Picture Soundtrack)',
-            albumart: 'albumart.jpg',
-            artists: 'Various Artists'
-        }
-    ]
-
-    const madeForYou = [
-        {
-            name: 'Galactic Odyssey',
-            albumart: 'albumart.jpg',
-            artists: 'Stellar Ensemble'
-        },
-        {
-            name: 'Lost in Time',
-            albumart: 'albumart.jpg',
-            artists: 'Chrono Wave'
-        },
-        {
-            name: 'Epic Journeys',
-            albumart: 'albumart.jpg',
-            artists: 'Quest Soundtrack'
-        },
-        {
-            name: 'City Lights',
-            albumart: 'albumart.jpg',
-            artists: 'Urban Grooves Collective'
-        },
-        {
-            name: 'Voyage to the Unknown',
-            albumart: 'albumart.jpg',
-            artists: 'Mystery Explorers'
-        },
-        {
-            name: 'Serenade of the Stars',
-            albumart: 'albumart.jpg',
-            artists: 'Celestial Harmony Orchestra'
-        },
-        {
-            name: 'Dreamscapes',
-            albumart: 'albumart.jpg',
-            artists: 'Somnolent Symphony'
-        },
-        {
-            name: 'Rhythms of the Cosmos',
-            albumart: 'albumart.jpg',
-            artists: 'Cosmic Percussionists'
-        }
-    ]
-
-
-
     return ( 
         <div className='main'>
             <Navbar />
-            <AlbumRow 
-                header = "Recently Played"
-                items = {recentlyPlayed}
-            />
-            <AlbumRow 
-                header = "Made For You"
-                items = {madeForYou}
-            />
-            <AlbumRow 
-                header = "Popular albums"
-                items = {madeForYou}
-            />
-            <AlbumRow 
-                header = "Discover picks for you"
-                items = {madeForYou}
-            />
+            <Home />
             <Footer />
         </div>
      );
