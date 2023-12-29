51d610d7c44b804be345bb259d041c55e141c325
Adithya S Sekhar <adithya0332@gmail.com>
2023-12-29 09:37:21 +0530

    AlbumDetails: Basic Structure 1

--- src/components/AlbumDetails/AlbumDetails.jsx
+++ src/components/AlbumDetails/AlbumDetails.jsx
@@ -1,11 +1,42 @@
-import './Album.css';
+import './AlbumDetails.css';
 
-const Album = () => {
+const AlbumDetails = (props) => {
+    const album = props.album;
+
     return ( 
         <div className="album-details">
-            
+            <div className="album-header">
+                <div className="album-header-art">
+                    <img src={album.albumart} />
+                </div>
+                <div className="album-header-text">
+                    <div className="album-header-category">
+                        {album.category}
+                    </div>
+                    <div className="album-header-title">
+                        {album.title}
+                    </div>
+                    <div className="album-header-details">
+                        <div className="album-header-artist-image">
+                            <div className="artist-image-mask">
+                                <img src={album.artistart} />
+                            </div>
+                        </div>
+                        <div className="album-header-summary">
+                            {album.artist} • {album.year} • {(album.songs).length}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="album-body">
+
+            </div>
+            <div className="album-footer">
+                {album.longDate}
+                {album.copyright}
+            </div>
         </div>
      );
 }
  
-export default Album;
+export default AlbumDetails;
