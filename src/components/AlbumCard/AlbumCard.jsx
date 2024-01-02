bf045adc045b3713bbce0b52bf9cf556b84e623c
Adithya S Sekhar <adithya0332@gmail.com>
2024-01-02 12:09:20 +0530

    AlbumCard: Add and enable play function

--- src/components/AlbumCard/AlbumCard.jsx
+++ src/components/AlbumCard/AlbumCard.jsx
@@ -1,15 +1,24 @@
 import { Link } from 'react-router-dom';
 import './AlbumCard.css';
+import { useContext } from 'react';
+import { CurrPlayingItemContext } from '../../contexts/CurrPlayingItemContext';
 
 const AlbumCard = (props) => {
     const album = props.album;
+
+    const currPlayingItem = useContext(CurrPlayingItemContext);
+
+    function playAlbum() {
+        currPlayingItem.setCurrPlayingAlbum(album);
+        currPlayingItem.setCurrPlayingSong(album.songs[0]);
+    }
 
     return ( 
         <Link to = {`/album/${ album.id }`}>    
             <div className="album-card">
                 <div className="album-card-art">
                     <img src={album.art} />
-                    <svg viewBox="0 0 24 24"><path d="m7.05 3.606 13.49 7.788a.7.7 0 0 1 0 1.212L7.05 20.394A.7.7 0 0 1 6 19.788V4.212a.7.7 0 0 1 1.05-.606z"></path></svg>
+                    <svg viewBox="0 0 24 24" onClick={playAlbum}><path d="m7.05 3.606 13.49 7.788a.7.7 0 0 1 0 1.212L7.05 20.394A.7.7 0 0 1 6 19.788V4.212a.7.7 0 0 1 1.05-.606z"></path></svg>
                 </div>
                 <div className='album-card-details'>
                     <div className="album-card-title">
