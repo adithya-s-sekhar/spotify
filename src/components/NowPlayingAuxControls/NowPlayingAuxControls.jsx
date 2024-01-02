85393e7e585178d948e4af53a4a8d7301c5e00d2
Adithya S Sekhar <adithya0332@gmail.com>
2024-01-02 11:35:16 +0530

    NowPlaying: Refactor: Extract Main and Aux Controls

---
+++ src/components/NowPlayingAuxControls/NowPlayingAuxControls.jsx
@@ -0,0 +1,11 @@
+import './NowPlayingAuxControls.css';
+
+const NowPlayingAuxControls = () => {
+    return ( 
+        <div className="np-aux-controls">
+            NP_AUX_CTRL
+        </div>
+     );
+}
+ 
+export default NowPlayingAuxControls;
