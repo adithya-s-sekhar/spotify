ec08c44d76b5e3cdcaa45215ead482e861445c87
Adithya S Sekhar <adithya0332@gmail.com>
2024-01-02 12:41:37 +0530

    albums: add more album art

--- src/albums.js
+++ src/albums.js
@@ -2,7 +2,7 @@ export const albums = [
     {
         id: 1,
         title: 'Revengers (Original Motion Picture Soundtrack)',
-        art: '/art.jpg',
+        art: '/art1.jpg',
         artist: 'Hans Dimmer',
         artistArt: '/artistart.jpg',
         category: 'Album',
@@ -40,7 +40,7 @@ export const albums = [
     {
         id: 2,
         title: 'SuperheroMovie 2: Revenge of Someone (Original Motion Picture Soundtrack)',
-        art: '/art.jpg',
+        art: '/art2.jpg',
         artist: 'Various Artists',
         artistArt: '/artistart.jpg',
         category: 'Album',
@@ -78,7 +78,7 @@ export const albums = [
     {
         id: 3,
         title: 'Galactic Odyssey',
-        art: '/art.jpg',
+        art: '/art3.jpg',
         artist: 'Stellar Ensemble',
         artistArt: '/artistart.jpg',
         category: 'Album',
@@ -115,7 +115,7 @@ export const albums = [
     {
         id: 4,
         title: 'Lost in Time',
-        art: '/art.jpg',
+        art: '/art4.jpg',
         artist: 'Chrono Wave',
         artistArt: '/artistart.jpg',
         category: 'Album',
@@ -152,7 +152,7 @@ export const albums = [
     {
         id: 5,
         title: 'Epic Journeys',
-        art: '/art.jpg',
+        art: '/art5.jpg',
         artist: 'Quest Soundtrack',
         artistArt: '/artistart.jpg',
         category: 'Album',
@@ -189,7 +189,7 @@ export const albums = [
     {
         id: 6,
         title: 'City Lights',
-        art: '/art.jpg',
+        art: '/art6.jpg',
         artist: 'Urban Grooves Collective',
         artistArt: '/artistart.jpg',
         category: 'Album',
@@ -226,7 +226,7 @@ export const albums = [
     {
         id: 7,
         title: 'Voyage to the Unknown',
-        art: '/art.jpg',
+        art: '/art7.jpg',
         artist: 'Mystery Explorers',
         artistArt: '/artistart.jpg',
         category: 'Album',
@@ -263,82 +263,8 @@ export const albums = [
     {
         id: 8,
         title: 'Serenade of the Stars',
-        art: '/art.jpg',
+        art: '/art8.jpg',
         artist: 'Celestial Harmony Orchestra',
-        artistArt: '/artistart.jpg',
-        category: 'Album',
-        year: '2014',
-        songs: [
-            {
-                songTitle: 'Song 1',
-                songFile: '/sample.mp3',
-                songArtist: 'Singer 1, Singer 2',
-                songDur: '5m'
-            },
-            {
-                songTitle: 'Song 2',
-                songFile: '/sample.mp3',
-                songArtist: 'Singer 2, Singer 3',
-                songDur: '4m'
-            },
-            {
-                songTitle: 'Song 3',
-                songFile: '/sample.mp3',
-                songArtist: 'Singer 3, Singer 4',
-                songDur: '5m'
-            },
-            {
-                songTitle: 'Song 4',
-                songFile: '/sample.mp3',
-                songArtist: 'Singer 4, Singer 1',
-                songDur: '3m'
-            }
-        ],
-        longDate: '01-Jan-2014',
-        copyright: 'Song Rights Company'
-    },
-    {
-        id: 9,
-        title: 'Dreamscapes',
-        art: '/art.jpg',
-        artist: 'Somnolent Symphony',
-        artistArt: '/artistart.jpg',
-        category: 'Album',
-        year: '2014',
-        songs: [
-            {
-                songTitle: 'Song 1',
-                songFile: '/sample.mp3',
-                songArtist: 'Singer 1, Singer 2',
-                songDur: '5m'
-            },
-            {
-                songTitle: 'Song 2',
-                songFile: '/sample.mp3',
-                songArtist: 'Singer 2, Singer 3',
-                songDur: '4m'
-            },
-            {
-                songTitle: 'Song 3',
-                songFile: '/sample.mp3',
-                songArtist: 'Singer 3, Singer 4',
-                songDur: '5m'
-            },
-            {
-                songTitle: 'Song 4',
-                songFile: '/sample.mp3',
-                songArtist: 'Singer 4, Singer 1',
-                songDur: '3m'
-            }
-        ],
-        longDate: '01-Jan-2014',
-        copyright: 'Song Rights Company'
-    },
-    {
-        id: 10,
-        title: 'Rhythms of the Cosmos',
-        art: '/art.jpg',
-        artist: 'Cosmic Percussionists',
         artistArt: '/artistart.jpg',
         category: 'Album',
         year: '2014',
