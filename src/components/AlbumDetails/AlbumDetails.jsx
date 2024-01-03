7c251c3060521ba93fdd5c18f273f4e67e114e1c
Adithya S Sekhar <adithya0332@gmail.com>
2024-01-03 20:41:43 +0530

    AlbumDetails: Enable Play button

--- src/components/AlbumDetails/AlbumDetails.jsx
+++ src/components/AlbumDetails/AlbumDetails.jsx
@@ -15,8 +15,22 @@ const AlbumDetails = () => {
     const currPlayingItem = useContext(CurrPlayingItemContext);
 
     function playAlbum() {
+        currPlayingItem.setIsPlaying(false);
         currPlayingItem.setCurrPlayingAlbum(album);
         currPlayingItem.setCurrPlayingSong(album.songs[0]);
+        currPlayingItem.setIsPlaying(true);
+    }
+
+    function pauseAlbum() {
+        currPlayingItem.setIsPlaying(false);
+    }
+
+    function togglePlay(){
+        if (currPlayingItem.isPlaying){
+            pauseAlbum();
+        } else {
+            playAlbum();
+        }
     }
 
     return ( 
@@ -45,8 +59,11 @@ const AlbumDetails = () => {
             <div className="album-body">
                 <div className="album-body-controls">
                     <div className="album-body-controls-left">
-                        <div className="album-body-controls-play" onClick={playAlbum}>
-                            <svg viewBox="0 0 24 24"><path d="m7.05 3.606 13.49 7.788a.7.7 0 0 1 0 1.212L7.05 20.394A.7.7 0 0 1 6 19.788V4.212a.7.7 0 0 1 1.05-.606z"></path></svg>
+                        <div className="album-body-controls-play" onClick={togglePlay}>
+                            <svg viewBox="0 0 24 24">
+                                {!currPlayingItem.isPlaying | currPlayingItem.currPlayingAlbum !== album && <path d="m7.05 3.606 13.49 7.788a.7.7 0 0 1 0 1.212L7.05 20.394A.7.7 0 0 1 6 19.788V4.212a.7.7 0 0 1 1.05-.606z"></path>}
+                                {currPlayingItem.isPlaying && currPlayingItem.currPlayingAlbum === album && <path d="M5.7 3a.7.7 0 0 0-.7.7v16.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V3.7a.7.7 0 0 0-.7-.7H5.7zm10 0a.7.7 0 0 0-.7.7v16.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V3.7a.7.7 0 0 0-.7-.7h-2.6z"></path>}
+                            </svg>
                         </div>
                         <div className="album-body-controls-heart">
                             <svg viewBox="0 0 24 24"><path d="M5.21 1.57a6.757 6.757 0 0 1 6.708 1.545.124.124 0 0 0 .165 0 6.741 6.741 0 0 1 5.715-1.78l.004.001a6.802 6.802 0 0 1 5.571 5.376v.003a6.689 6.689 0 0 1-1.49 5.655l-7.954 9.48a2.518 2.518 0 0 1-3.857 0L2.12 12.37A6.683 6.683 0 0 1 .627 6.714 6.757 6.757 0 0 1 5.21 1.57zm3.12 1.803a4.757 4.757 0 0 0-5.74 3.725l-.001.002a4.684 4.684 0 0 0 1.049 3.969l.009.01 7.958 9.485a.518.518 0 0 0 .79 0l7.968-9.495a4.688 4.688 0 0 0 1.049-3.965 4.803 4.803 0 0 0-3.931-3.794 4.74 4.74 0 0 0-4.023 1.256l-.008.008a2.123 2.123 0 0 1-2.9 0l-.007-.007a4.757 4.757 0 0 0-2.214-1.194z"></path></svg>
