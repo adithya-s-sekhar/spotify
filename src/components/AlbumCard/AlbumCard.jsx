8ae9a65c496ec1d1c835ec32660fb6462389aeea
Adithya S Sekhar <adithya0332@gmail.com>
2024-01-03 20:36:44 +0530

    AlbumCard: Enable Play button

--- src/components/AlbumCard/AlbumCard.jsx
+++ src/components/AlbumCard/AlbumCard.jsx
@@ -7,27 +7,46 @@ const AlbumCard = ({album}) => {
     const currPlayingItem = useContext(CurrPlayingItemContext);
 
     function playAlbum() {
+        currPlayingItem.setIsPlaying(false);
         currPlayingItem.setCurrPlayingAlbum(album);
         currPlayingItem.setCurrPlayingSong(album.songs[0]);
+        currPlayingItem.setIsPlaying(true);
     }
 
-    return ( 
-        <Link to = {`/album/${ album.id }`}>    
+    function pauseAlbum() {
+        currPlayingItem.setIsPlaying(false);
+    }
+
+    function togglePlay(){
+        if (currPlayingItem.isPlaying){
+            pauseAlbum();
+        } else {
+            playAlbum();
+        }
+    }
+
+    return (   
             <div className="album-card">
                 <div className="album-card-art">
+                    <Link to = {`/album/${ album.id }`}>  
                     <img src={album.art} />
-                    <svg viewBox="0 0 24 24" onClick={playAlbum}><path d="m7.05 3.606 13.49 7.788a.7.7 0 0 1 0 1.212L7.05 20.394A.7.7 0 0 1 6 19.788V4.212a.7.7 0 0 1 1.05-.606z"></path></svg>
+                    </Link>
+                    <svg viewBox="0 0 24 24" onClick={togglePlay}>
+                        {!currPlayingItem.isPlaying | currPlayingItem.currPlayingAlbum !== album && <path d="m7.05 3.606 13.49 7.788a.7.7 0 0 1 0 1.212L7.05 20.394A.7.7 0 0 1 6 19.788V4.212a.7.7 0 0 1 1.05-.606z"></path>}
+                        {currPlayingItem.isPlaying && currPlayingItem.currPlayingAlbum === album && <path d="M5.7 3a.7.7 0 0 0-.7.7v16.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V3.7a.7.7 0 0 0-.7-.7H5.7zm10 0a.7.7 0 0 0-.7.7v16.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V3.7a.7.7 0 0 0-.7-.7h-2.6z"></path>}
+                    </svg>
                 </div>
-                <div className='album-card-details'>
-                    <div className="album-card-title">
-                        {album.title}
+                <Link to = {`/album/${ album.id }`}>  
+                    <div className='album-card-details'>
+                        <div className="album-card-title">
+                            {album.title}
+                        </div>
+                        <div className="album-card-artists">
+                            {album.artist}
+                        </div>
                     </div>
-                    <div className="album-card-artists">
-                        {album.artist}
-                    </div>
-                </div>
+                </Link>
             </div>
-        </Link>
      );
 }
  
