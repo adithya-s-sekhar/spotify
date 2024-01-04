1a4bffa93001ceef675f93b70ad1940afde2723f
Adithya S Sekhar <adithya0332@gmail.com>
2024-01-04 10:59:16 +0530

    AlbumSong: Make song list visually respond to clicks

--- src/components/AlbumSong/AlbumSong.jsx
+++ src/components/AlbumSong/AlbumSong.jsx
@@ -1,19 +1,48 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import './AlbumSong.css';
 import { CurrPlayingItemContext } from '../../contexts/CurrPlayingItemContext';
 
 const AlbumSong = ({index, songTitle, songArtist, songDur, album}) => {
     const currPlayingItem = useContext(CurrPlayingItemContext);
 
-    function changeSong() {
+    function playSong() {
+        let element = document.getElementsByClassName('album-body-song-active')[0];
+        if (element) {element.classList.remove("album-body-song-active")}
         currPlayingItem.setIsPlaying(false);
         currPlayingItem.setCurrPlayingAlbum(album);
         currPlayingItem.setCurrPlayingSong(album.songs[index]);
         currPlayingItem.setIsPlaying(true);
+        if ((index+1) == currPlayingItem.currPlayingSong.songIndex){
+            let element = document.getElementsByClassName('song'+(index+1))[0];
+            element.classList.add("album-body-song-active");
+        }
     }
 
+    function pauseSong() {  
+        currPlayingItem.setIsPlaying(false);
+    }
+
+    function togglePlay(){
+        if (currPlayingItem.isPlaying){
+            if (currPlayingItem.currPlayingSong.songIndex === (index + 1)){
+                pauseSong();
+            } else {
+                playSong();
+            }
+        } else {
+            playSong();
+        }
+    }
+
+    useEffect(() => {
+        if ((index+1) == currPlayingItem.currPlayingSong.songIndex){
+            let element = document.getElementsByClassName('song'+(index+1))[0];
+            element.classList.add("album-body-song-active");
+        }
+    },[currPlayingItem.currPlayingSong])
+
     return (
-        <div className="album-body-song" onClick={changeSong}>
+        <div className={`album-body-song song` + (index+1)} onClick={togglePlay}>
             <div className="album-body-song-index">
                 {index + 1}
             </div>
