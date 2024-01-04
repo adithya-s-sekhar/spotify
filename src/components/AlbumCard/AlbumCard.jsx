f0f8b8424a5e5fdfdc08ad621ab139638424aebd
Adithya S Sekhar <adithya0332@gmail.com>
2024-01-04 16:01:22 +0530

    AlbumCard: always show pause button when an album is playing

--- src/components/AlbumCard/AlbumCard.jsx
+++ src/components/AlbumCard/AlbumCard.jsx
@@ -31,7 +31,10 @@ const AlbumCard = ({album}) => {
                     <Link to = {`/album/${ album.id }`}>  
                     <img src={album.art} />
                     </Link>
-                    <svg viewBox="0 0 24 24" onClick={togglePlay}>
+                    <svg viewBox="0 0 24 24" onClick={togglePlay} style={{
+                        opacity: (currPlayingItem.isPlaying && currPlayingItem.currPlayingAlbum === album) ? '1' : '0',
+                        bottom: (currPlayingItem.isPlaying && currPlayingItem.currPlayingAlbum === album) ? '8%' : '0',
+                    }}>
                         {!currPlayingItem.isPlaying | currPlayingItem.currPlayingAlbum !== album && <path d="m7.05 3.606 13.49 7.788a.7.7 0 0 1 0 1.212L7.05 20.394A.7.7 0 0 1 6 19.788V4.212a.7.7 0 0 1 1.05-.606z"></path>}
                         {currPlayingItem.isPlaying && currPlayingItem.currPlayingAlbum === album && <path d="M5.7 3a.7.7 0 0 0-.7.7v16.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V3.7a.7.7 0 0 0-.7-.7H5.7zm10 0a.7.7 0 0 0-.7.7v16.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V3.7a.7.7 0 0 0-.7-.7h-2.6z"></path>}
                     </svg>
